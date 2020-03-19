6282a2fd12779631a419b4d54de83689169cf793
Julien Tinguely <julien.tinguely@gmail.com>
2020-03-19 10:24:06 +0100

    merged with normalized laplacian

--- graph.c
+++ graph.c
@@ -43,37 +43,94 @@ static void construct_eps_neighborhood_matrix(double *points, int lines, int dim
     }
 }
 
-/*
- * compute degrees matrix D
- */
-static double compute_degrees(double *graph, double *degrees, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (graph[i*n+j] > 0.0) {
-                degrees[i*n+i] += graph[i*n+j];
-            } else {
-                degrees[i*n+i] += 0;
+static void calculate_diagonal_degree_matrix(double *d, double * weighted_adj_matrix, int n){
+    for (int i = 0; i< n; i++){
+        double d_i = 0;
+        for (int j = 0; j<n;j++){
+            d_i += weighted_adj_matrix[i*n+ j];
+        }
+        d[i] = d_i;
+    }
+}
+
+static void construct_normalized_laplacian_sym_matrix(double *l_sym, double * weighted_adj_matrix, int num_points){
+    double * sqrt_inv_degree_matrix;  // '1-d' array
+    calculate_diagonal_degree_matrix(sqrt_inv_degree_matrix, weighted_adj_matrix, num_points); //load degree_matrix temporarily in sqrt_inv_degree_matrix
+    for (int i =0; i < num_points; i++){
+        sqrt_inv_degree_matrix[i] = 1.0/sqrt(sqrt_inv_degree_matrix[i]);
+    }
+    // compute D^(-1/2) W,  not sure if this code is optimal yet, how to avoid "jumping row"?  process one row each time enccourage spatial locality
+    // but with this trick we avoid *0.0
+    for (int i = 0; i < num_points; i++){
+        for(int j = 0; j < num_points; j++){
+            l_sym[i*num_points + j] = sqrt_inv_degree_matrix[i]*weighted_adj_matrix[(i+j)*num_points + j];
+        }
+    }
+    // compute (D^(-1/2)*W)*D^(-1/2)
+    for(int i = 0; i< num_points; i++){
+        for(int j = 0; j< num_points; j++){
+            if(i==j){
+                l_sym[i*num_points + j] = 1.0 - l_sym[i*num_points + j]*sqrt_inv_degree_matrix[j];
+            }else{
+                l_sym[i*num_points + j] = - l_sym[i*num_points + j]*sqrt_inv_degree_matrix[j];
             }
         }
     }
-//    for (int i = 0; i < n; i++) {
-//        for (int j = 0; j < n; j++) {
-//            printf("%f ", degrees[i*n+ j]);
-//        }
-//        printf("\n");
-//    }
 }
 
-/*
- * compute unnormalized graph laplacian
- */
+static void construct_normalized_laplacian_rw_matrix(double *l_rw, double * weighted_adj_matrix,  int num_points){
+    double * inv_degree_matrix;
+    calculate_diagonal_degree_matrix(inv_degree_matrix, weighted_adj_matrix, num_points); //load degree_matrix temporarily in sqrt_inv_degree_matrix
+    for (int i =0; i < num_points; i++){
+        inv_degree_matrix[i] = 1.0/inv_degree_matrix[i]);
+    }
+    for (int i = 0; i < num_points; i++){
+        for(int j = 0; j < num_points; j++){
+            if(i==j){
+                l_rw[i*num_points + j] = 1.0 - inv_degree_matrix[i]*weighted_adj_matrix[(i+j)*num_points + j];
+            }else{
+                l_rw[i*num_points + j] =  - inv_degree_matrix[i]*weighted_adj_matrix[(i+j)*num_points + j];
+            }
+        }
+    }
+}
+
 static void construct_unnormalized_laplacian(double *graph, double *laplacian, int n) {
     double* degrees = (double *)malloc(n * n * sizeof(double));
-    compute_degrees(graph, degrees, n);
+    calculate_diagonal_degree_matrix(graph, degrees, n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             laplacian[i*n+j] = degrees[i*n+j] - graph[i*n+j];
             printf("%f ", laplacian[i*n+j]);
+        }
+        printf("\n");
+    }
+}
+
+static void construct_knn_matrix(double *points, int lines, int dim, int k, int *ret) {
+    double vals[lines];
+    int indices[lines];
+
+    for (int i = 0; i < lines; ++i) {
+        for(int ii = 0; ii < lines; ++ii) {
+            indices[ii] = ii;
+        }
+        for (int j = 0; j < lines; ++j) {
+            ret[i*lines + j] = 0.0;
+            vals[j] = l2_norm(&points[i*dim], &points[j*dim], dim);
+        }
+        TheArray = vals;
+        size_t len = sizeof(vals)/ sizeof(*vals);
+        qsort(indices, len, sizeof(*indices), cmp);
+        for (int s = 0; s < k; ++s) {
+            ret[i*lines + indices[s]] = 1.0;
+        }
+    }
+
+    for(int i = 0; i < lines; ++i) {
+        for(int j = 0; j < lines; ++j) {
+            printf("%d ", ret[i*lines + j]);
+
         }
         printf("\n");
     }
