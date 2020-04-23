9a4a3b28a0d2f5280a755da91343d8b86b6b68ab
Pouya <devpouya@gmail.com>
2020-04-23 11:16:39 +0200

    make circles

--- generate_gaussian.py
+++ generate_gaussian.py
@@ -1,11 +1,16 @@
 import numpy as np
-from sklearn.datasets import make_blobs
+from sklearn.datasets import make_blobs, make_circles
 
 k = int(input("enter desired number of clusters: "))
 n = int(input("enter desired number of total data points: "))
 
 
-points, y = make_blobs(n_samples=n,centers=k,n_features=2,random_state=7)
+#points, y = make_blobs(n_samples=n,centers=k,cluster_std=0.5,n_features=2,random_state=7)
+
+points, y = make_circles(n_samples=n)
+
+print(points.shape)
+
 """
 means = np.random.choice(n, (k,1,2), replace=False)
 print(means)
@@ -23,7 +28,7 @@ for i in range(k):
     points = np.append(points,st,axis=0)
 np.random.shuffle(points)
 """
-file = open("./gaussian_2d_nk.txt","w+")
+file = open("10c.txt", "w+")
 file.write("2\n")
 for i in range(n):
     for j in range(2):
