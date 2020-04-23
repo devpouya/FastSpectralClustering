b5f01e0d9d232ac5415f08b9f1526756e8cc97e6
Pouya <devpouya@gmail.com>
2020-04-23 11:27:22 +0200

    circles

--- generate_circles.py
+++ generate_circles.py
@@ -11,23 +11,7 @@ points, y = make_circles(n_samples=n)
 
 print(points.shape)
 
-"""
-means = np.random.choice(n, (k,1,2), replace=False)
-print(means)
-cov = [[1, 0], [0, 1]]
 
-g = np.random.gamma(range(k+1),k+1)
-g /= np.sum(g)
-g *= n
-g = np.rint(g)[1:]
-print(g)
-
-points = np.empty((n,2))
-for i in range(k):
-    st = np.random.multivariate_normal(means[i].reshape(2,), cov, int(g[i]))
-    points = np.append(points,st,axis=0)
-np.random.shuffle(points)
-"""
 file = open("10c.txt", "w+")
 file.write("2\n")
 for i in range(n):
