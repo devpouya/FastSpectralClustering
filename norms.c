98c2aeb3c4c8dd7090387d00962681929211e6bb
Pouya <devpouya@gmail.com>
2020-05-07 14:51:55 +0200

    fast schraudolph exp, correct (gaussian2d, 5c)

--- norms.c
+++ norms.c
@@ -3,30 +3,49 @@
 #include "instrumentation.h"
 
 
-double appx_exp(double x) {
+double fast_LUT_exp(double x) {
     ENTER_FUNC;
-    NUM_ADDS(5);
-    NUM_MULS(14);
+    NUM_ADDS(1);
+    NUM_MULS(2);
+    uint64_t tmp = (1512775*x+1072632447);
+    int index = (int) (tmp>>12) & 0xFF;
     EXIT_FUNC;
-    return 1 + x + x*x*0.5 + x*x*x*0.166 + x*x*x*x*0.04166 + x*x*x*x*x*0.00833;
+    return ((double )(tmp<<32 ))* ADJUSTMENT_LUT[index];
 }
 
-double appx_sqrt_babylonian(double x) {
+double fast_exp(double x) {
     ENTER_FUNC;
-    NUM_MULS(100);
-    NUM_ADDS(100);
-    NUM_DIVS(100);
-    double xx = x;
-    double y = 1.0;
-    // double acc = 0.0001; // find a way to use this and measure flops
-    // while (x-y>acc)
-    for(int i = 0; i < 100; i++) {
-        xx = (xx+y)*0.5;
-        y = x/xx;
-    }
+    NUM_MULS(2);
+    NUM_DIVS(1);
+    NUM_ADDS(2);
+    int x1 = (long long) (6051102*x+1056478197);
+    int x2 = (long long) (1056478197-6051102*x);
     EXIT_FUNC;
-    return xx;
+    return ((double) x1)/((double) x2);
 }
+
+double fast_schraudolph_exp(double x) {
+    ENTER_FUNC;
+    NUM_MULS(3);
+    NUM_ADDS(1);
+    static union {
+        double d;
+        struct{
+#ifdef  LITTLE_ENDIAN
+            int i, j;
+#else
+            int j, i;
+#endif
+
+        } n; } eco;
+
+    eco.n.i = 1512775.3951951856938*x +1072632447;
+    EXIT_FUNC;
+    return eco.d;
+
+
+}
+
 
 double l2_norm(double *u, double *v, int dim) {
     ENTER_FUNC;
@@ -42,18 +61,7 @@ double l2_norm(double *u, double *v, int dim) {
     return norm;
 }
 
-double appx_l2_norm(double *u, double *v, int dim) {
-    ENTER_FUNC;
-    NUM_ADDS(3*dim);
-    NUM_MULS(dim);
-    double norm = 0;
-    for (int i = 0; i < dim; i++) {
-        norm += (u[i] - v[i]) * (u[i] - v[i]);
-    }
-    norm = appx_sqrt_babylonian(norm);
-    EXIT_FUNC;
-    return norm;
-}
+
 
 double l2_norm_squared(double *u, double *v, int dim) {
     ENTER_FUNC;
@@ -67,7 +75,7 @@ double l2_norm_squared(double *u, double *v, int dim) {
     return norm;
 }
 
-/*
+
 double gaussian_similarity(double *u, double *v, int dim) {
     ENTER_FUNC;
     NUM_EXPS(1);
@@ -77,12 +85,14 @@ double gaussian_similarity(double *u, double *v, int dim) {
     return inner;
 }
 
-*/
-double gaussian_similarity(double *u, double *v, int dim) {
+
+
+
+
+double fast_gaussian_similarity(double *u, double *v, int dim) {
     ENTER_FUNC;
-    NUM_DIVS(0);
-    NUM_MULS(5);
-    double inner = /*appx_*/exp(-0.5 * l2_norm_squared(u, v, dim));
+    NUM_MULS(1);
+    double inner = fast_schraudolph_exp(-0.5 * l2_norm_squared(u, v, dim));
     EXIT_FUNC;
     return inner;
 }
