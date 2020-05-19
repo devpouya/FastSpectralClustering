e5f8058e86fc53795a38d242ca879dea2d0e6908
wangzu <wangzu@student.ethz.ch>
2020-05-19 11:01:27 +0200

    update

--- scikit_measure.py
+++ scikit_measure.py
@@ -6,28 +6,81 @@ import random
 import os
 from sklearn.cluster import SpectralClustering
 from time import perf_counter
+import subprocess
+import os
+import csv
 
-dataset_path = "datasets/test_points/"+input("enter the dataset file name (from datasets/test_points/): ")
-number_clusters = input("enter number of clusters  ")
-output_path = "output/validation/"+input("enter the output file name (result in /output/validation/): ")
+# global settings
+num_runs = 1
+median_idx = 0
+
+# First Benchmark: growing dim
+test = "growing dim"
+output_filename = "vec_8.txt"
+dataset_path = os.getcwd() + "/benchmarks/datasets/6c_5000n_growing_dim/"
+output_path = os.getcwd() + "/benchmarks/kmeans/measurements/"
+k = 6
+n = 5000
+params = [2, 4, 8, 16, 32, 64, 128, 256, 512]
+
+# # Second Benchmark: growing n
+# test = "growing n"
+# output_filename = "vec_8.txt"
+# dataset_path = os.getcwd() + "benchmarks/datasets/growing_n/"
+# output_path = os.getcwd() + "/benchmarks/graph/measurements/"
+# k = 6 #??
+# dim = 2 #??
+# n = range(10, 10000, 100)
+
+# global
+
+directory = os.fsencode(str(dataset_path))
+runtimes_median = []
+performances_median = []
 
 
-f_input = open(dataset_path, "r")
+# dataset_path = "datasets/test_points/"+input("enter the dataset file name (from datasets/test_points/): ")
+# number_clusters = input("enter number of clusters  ")
+# output_path = "output/validation/"+input("enter the output file name (result in /output/validation/): ")
+#
 
-data = f_input.read().split()
-dim = int(data[0])
-data = data[1:]
-data_np = np.array(data).reshape(-1, dim)
+def load_data(dataset_path):
+    f_input = open(dataset_path, "r")
+    data = f_input.read().split()
+    dim = int(data[0])
+    data = data[1:]
+    data_np = np.array(data).reshape(-1, dim)
+    return data_np
 
-number_clusters = int(number_clusters)
+for par in params:
+    print(test + " | parameter = " + str(par))
+    for file in sorted(os.listdir(directory)): # when generating data have just numbers for simplicity
+        filename = os.fsdecode(file)
+        # print("filename: {}".format(filename))
+        if filename == str(par) + ".txt":
+            # compute NUM_RUNS times and get the median
+            runtimes = []
+            for i in range(0, num_runs):
+                data_np = load_data(dataset_path + filename)
+                t1_start = perf_counter()
+                clustering_result, time_ar = \
+                                    SpectralClustering(n_clusters=k, eigen_solver='arpack', n_components=k, random_state=30,
+                                                       n_init=1, gamma=1.0, affinity='rbf',  eigen_tol=1e-12,
+                                                       assign_labels='kmeans', kernel_params=None, n_jobs=1, kmeans_algo='full').fit_predict(data_np)
+                t1_stop = perf_counter()
+                runtime_sec =  t1_stop-t1_start-time_ar
+                runtimes.append(runtime_sec)
+            # sort the arrays
+            runtimes.sort()
+            # adding to final list
+            runtimes_median.append(runtimes[median_idx])
 
-t1_start = perf_counter()
-for i in range(1):
-    clustering_result = \
-        SpectralClustering(n_clusters=number_clusters, eigen_solver='arpack', n_components=number_clusters, random_state=30,
-                           n_init=1, gamma=1.0, affinity='rbf',  eigen_tol=1e-12,
-                           assign_labels='kmeans', kernel_params=None, n_jobs=1, kmeans_algo='full').fit_predict(data_np)
-t1_stop = perf_counter()
-print("Elapsed time during the whole program in seconds:",
-      (t1_stop-t1_start)/1.0)
+            print("runtime: "+ str(runtimes[median_idx]) +" (sec)")
+
+
+with open(str(output_path) + output_filename + "_runtime", 'w') as f:
+    writer = csv.writer(f, delimiter='\t')
+    writer.writerows(zip(params, runtimes_median))
+
+
 
