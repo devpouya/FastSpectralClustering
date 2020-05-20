aa7eae6b5e904d72bf3b95461880a3fff899981c
wangzu <wangzu@student.ethz.ch>
2020-05-20 10:40:12 +0200

    update scikit

--- scikit_measure.py
+++ scikit_measure.py
@@ -14,18 +14,28 @@ import csv
 num_runs = 3
 median_idx = 1
 
-# First Benchmark: growing dim
-test = "growing dim"
+# # First Benchmark: growing dim
+# test = "growing dim"
+# output_filename = ""
+# dataset_path = os.getcwd() + "/benchmarks/datasets/6c_5000n_growing_dim/"
+# output_path = os.getcwd() + "/benchmarks/scikit/measurements/"
+# k = 6
+# n = 5000
+#
+# params = [2, 4]
+
+# second Benchmark: growing k
+test = "growing k"
 output_filename = ""
-dataset_path = os.getcwd() + "/benchmarks/datasets/6c_5000n_growing_dim/"
-output_path = os.getcwd() + "/benchmarks/scikit/measurements/"
-k = 6
-n = 5000
+dataset_path = os.getcwd() + "/benchmarks/datasets/2d_2500n_growing_k/"
+output_path = os.getcwd() + "/benchmarks/scikit/measurements/growing_k/"
 
-params = [2, 4]
+n = 2500
+# params = [2]
+params = [i for i in range(2,100)]
 
 
-# # Second Benchmark: growing n
+# # Third Benchmark: growing n
 # test = "growing n"
 # output_filename = "vec_8.txt"
 # dataset_path = os.getcwd() + "benchmarks/datasets/growing_n/"
@@ -57,55 +67,49 @@ for par in params:
         if filename == str(par) + ".txt":
             # compute NUM_RUNS times and get the median
             runtimes = []
-            runtimes2 = []
-            runtimes3 = []
+            # runtimes2 = []
+            # runtimes3 = []
+            k = par
+            print("number of clusters : {}".format(k))
             for i in range(0, num_runs):
                 data_np = load_data(dataset_path + filename)
-                t1_start = perf_counter()
-                clustering_result, time_ar = \
+                # t1_start = perf_counter()
+                clustering_result, time_kmeans = \
                                     SpectralClustering(n_clusters=k, eigen_solver='arpack', n_components=k, random_state=30,
                                                        n_init=1, gamma=1.0, affinity='rbf',  eigen_tol=1e-12,
                                                        assign_labels='kmeans', kernel_params=None, n_jobs=1, kmeans_algo='full').fit_predict(data_np)
-                t1_stop = perf_counter()
-                runtime_sec = t1_stop-t1_start-time_ar
-                runtimes.append(runtime_sec)
-                runtimes3.append(t1_stop-t1_start)
-
-                t1_start = perf_counter()
-                clustering_result, time_ar = \
-                                    SpectralClustering(n_clusters=k, eigen_solver='arpack', n_components=k, random_state=30,
-                                                       n_init=1, gamma=1.0, affinity='rbf',  eigen_tol=1e-12,
-                                                       assign_labels='kmeans', kernel_params=None, n_jobs=1, kmeans_algo='elkan').fit_predict(data_np)
-                t1_stop = perf_counter()
-                runtime_sec = t1_stop-t1_start-time_ar
-                runtimes2.append(runtime_sec)
+                # print(clustering_result)
+                # print(clustering_result.shape)
+                # t1_stop = perf_counter()
+                runtimes.append(time_kmeans)
+                # runtimes3.append(t1_stop-t1_start)
 
             # sort the arrays
             runtimes.sort()
-            runtimes2.sort()
-            runtimes3.sort()
+            # runtimes2.sort()
+            # runtimes3.sort()
             # adding to final list
             runtimes_median.append(runtimes[median_idx])
-            elkan_runtimes_median.append(runtimes2[median_idx])
-            total_runtimes_median.append(runtimes3[median_idx])
+            # elkan_runtimes_median.append(runtimes2[median_idx])
+            # total_runtimes_median.append(runtimes3[median_idx])
 
-            print("runtime lloyd no_eig: "+ str(runtimes[median_idx]) +" (sec)")
-            print("runtime elkan no_eig: " + str(runtimes2[median_idx]) + " (sec)")
-            print("runtime lloyd with_eig: " + str(runtimes3[median_idx]) + " (sec)")
-            print("runtime eig: " + str(runtimes3[median_idx] - runtimes[median_idx]) + " (sec)")
+            print("runtime lloyd kmeans part: "+ str(runtimes[median_idx]) +" (sec)")
+            # print("runtime elkan no_eig: " + str(runtimes2[median_idx]) + " (sec)")
+            # print("runtime lloyd with_eig: " + str(runtimes3[median_idx]) + " (sec)")
+            # print("runtime eig: " + str(runtimes3[median_idx] - runtimes[median_idx]) + " (sec)")
 
 
-with open(str(output_path)+ "_total_lloyd_runtime_no_eig", 'w') as f:
+with open(str(output_path)+ "runtime_lloyd_kmeans_part", 'w') as f:
     writer = csv.writer(f, delimiter='\t')
     writer.writerows(zip(params, runtimes_median))
-
-with open(str(output_path) + "_elkan_runtime_no_eig", 'w') as f:
-    writer = csv.writer(f, delimiter='\t')
-    writer.writerows(zip(params, elkan_runtimes_median))
-
-with open(str(output_path) + "_total_lloyd_runtime_with_eig", 'w') as f:
-    writer = csv.writer(f, delimiter='\t')
-    writer.writerows(zip(params, total_runtimes_median))
-
-
+#
+# with open(str(output_path) + "_elkan_runtime_no_eig", 'w') as f:
+#     writer = csv.writer(f, delimiter='\t')
+#     writer.writerows(zip(params, elkan_runtimes_median))
+#
+# with open(str(output_path) + "_total_lloyd_runtime_with_eig", 'w') as f:
+#     writer = csv.writer(f, delimiter='\t')
+#     writer.writerows(zip(params, total_runtimes_median))
+#
+#
 
